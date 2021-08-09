42985ee741591a2303c3a946c2e46d80a0abd55d
abdelrahman2500 <abdelrahman.kh216@yahoo.com>
2021-08-10 01:03:11 +0200

    setting error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.scss';
 import { ContextProvider } from './context/Context';
 import { useState } from 'react';
-import Branches from './components/barnches/branches';
 
 function App() {
 
@@ -11,7 +10,7 @@ const[lang, setLang]= useState(localStorage.getItem("lang"))
   return (
     <div className="App">
       <ContextProvider>
-        <Branches />
+      
       </ContextProvider>
     </div>
   );
