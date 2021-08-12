298ca3014e85e71df624b3762267bf6f6726aaf3
abdelrahman2500 <abdelrahman.kh216@yahoo.com>
2021-08-12 14:14:18 +0200

    adding shopAr

---
+++ src/components/sidebar/SidebarAr.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function SidebarAr() {
+    return (
+        <div className="sidebar">
+            {/* categories */}
+
+            {/* brands */}
+
+            
+        </div>
+    )
+}
