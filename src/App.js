6d1139905c1fdd1da0c86b29823842c54c147ec7
abdelrahman2500 <abdelrahman.kh216@yahoo.com>
2021-08-12 15:33:21 +0200

    updating app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import "./App.scss";
 import { useContext } from "react";
 import { Context } from "./context/Context";
 import Home from "./pages/home/Home";
 import HomeAr from "./pages/home/HomeAr";
+import { Route, Switch } from "react-router-dom";
+import ForPharmacies from './pages/pharmacies/ForPharmacies';
 
 function App() {
   const context = useContext(Context);
@@ -13,12 +14,56 @@ function App() {
       {context.lang === "en" ? (
         <div className="AppEn">
           {/* hello english */}
-          <Home />
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/pharamcies" exact>
+              <ForPharmacies />
+            </Route>
+            <Route path="/insurers" exact>
+              <Home />
+            </Route>
+            <Route path="/doctors" exact>
+              <Home />
+            </Route>
+            <Route path="/about" exact>
+              <Home />
+            </Route>
+            <Route path="/blog" exact>
+              <Home />
+            </Route>
+            <Route path="/contact" exact>
+              <Home />
+            </Route>
+          </Switch>
         </div>
       ) : (
         <div className="AppAr" dir="rtl">
           {/* hello arabic */}
-          <HomeAr />
+          <Switch>
+            <Route path="/" exact>
+              <HomeAr />
+            </Route>
+            <Route path="/pharamcies" exact>
+              <ForPharmacies />
+            </Route>
+            <Route path="/insurers" exact>
+              <HomeAr />
+            </Route>
+            <Route path="/doctors" exact>
+              <HomeAr />
+            </Route>
+            <Route path="/about" exact>
+              <HomeAr />
+            </Route>
+            <Route path="/blog" exact>
+              <HomeAr />
+            </Route>
+            <Route path="/contact" exact>
+              <HomeAr />
+            </Route>
+          </Switch>
         </div>
       )}
     </div>
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
 
 export default function Shop() {
     return (
         <div className="shop">
             {/* navbar */}
-
+                <Navbar />
             {/* barnches */}
 
             {/* sidebar and products */}
