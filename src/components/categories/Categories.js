f169d774268c81d877aceb99c517e9d3967c991a
ma7moudemam <m7moud_emam@hotmail.com>
2021-08-14 06:27:38 +0200

    add Categories

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import "./index.scss";
+
+export default function Categories(){
+    return(
+        <div class="container">
+            <h1 class="header">
+            Categories
+            </h1>
+        </div>
+    );
+}
